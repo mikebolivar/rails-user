b98b248454bb4cfc240f0d28de859ed9657b0d79
Miguel Bolivar <m87.bolivar@gmail.com>
2014-12-13 10:57:08 -0600

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212022703) do
+ActiveRecord::Schema.define(version: 20141213164401) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141212022703) do
     t.string   "facebook"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
