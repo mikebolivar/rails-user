a727c5c5c23c4e5f7f669e154796762bb56f2560
Miguel Bolivar <m87.bolivar@gmail.com>
2014-12-13 12:42:53 -0600

    Fixed the confirmation password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        flash[:success] = "Profile updated"
+        format.html { redirect_to @user }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -71,6 +72,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :encrypted_password)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "fo1obar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
 end
